5524cced311607027d38a16103a151a7876cd9ce
Gavin Borne <gavin.bor0925@gmail.com>
2025-12-23 02:10:28 -0500

    remove confirmation view (for now)

--- CatBot/discord/views/__init__.py
+++ CatBot/discord/views/__init__.py
@@ -3,7 +3,6 @@
 __author__ = "Gavin Borne"
 __license__ = "MIT"
 
-__all__ = ["ConfirmationView", "RestrictedModal", "RestrictedView"]
+__all__ = ["RestrictedModal", "RestrictedView"]
 
-from .confirmation_view import ConfirmationView
 from .restricted_view import RestrictedModal, RestrictedView
--- CatBot/discord/views/__init__.pyi
+++ CatBot/discord/views/__init__.pyi
@@ -5,7 +5,6 @@ __license__: Final[str]
 
 __all__: Final[list[str]]
 
-from .confirmation_view import ConfirmationView as ConfirmationView
 from .restricted_view import (
     RestrictedModal as RestrictedModal,
     RestrictedView as RestrictedView,
